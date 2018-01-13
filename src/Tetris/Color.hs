272507d6bf5bfaf87d15733ee1ea0ee8a61c8101
Fabian Geiselhart <me@f4814n.de>
2018-01-13 14:52:20 +0100

    Use deriving on Color type

--- src/Tetris/Color.hs
+++ src/Tetris/Color.hs
@@ -3,16 +3,4 @@ module Tetris.Color
     ) where
 
 data Color = Red | Blue | White | Yellow | Magenta | Cyan | Green | Black
-    deriving (Eq)
-
-instance Show Color where
-    show x =
-        case x of
-            Red     -> "Red"
-            Blue    -> "Blue"
-            White  -> "White"
-            Yellow  -> "Yellow"
-            Magenta -> "Magenta"
-            Cyan    -> "Cyan"
-            Green   -> "Green"
-            Black   -> "Black"
+    deriving (Eq, Show)
